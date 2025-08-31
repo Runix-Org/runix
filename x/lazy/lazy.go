e660c2ad0a64f5ea66f584adf6cf40d1b81bfda6
ReanGD <reangd@yandex.ru>
2025-08-31 04:55:41 +0300

    add lazy package for cache

---
+++ x/lazy/lazy.go
@@ -0,0 +1,69 @@
+package lazy
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type state uint32
+
+const (
+	stNew state = iota
+	stOK
+	stFailed
+)
+
+type Value[T any] struct {
+	mu     sync.Mutex
+	st     atomic.Uint32
+	val    T
+	name   string
+	initFn func() (T, error)
+}
+
+func New[T any](name string, initFn func() (T, error)) *Value[T] {
+	return &Value[T]{name: name, initFn: initFn}
+}
+
+func (v *Value[T]) Init() (err error) {
+	if state(v.st.Load()) != stNew {
+		panic(fmt.Sprintf("double init of module %s", v.name))
+	}
+
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	if state(v.st.Load()) != stNew {
+		panic(fmt.Sprintf("double init of module %s", v.name))
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			v.st.Store(uint32(stFailed))
+			panic(r)
+		}
+	}()
+
+	val, err := v.initFn()
+	if err != nil {
+		v.st.Store(uint32(stFailed))
+		return err
+	}
+
+	v.val = val
+	v.st.Store(uint32(stOK))
+
+	return nil
+}
+
+func (v *Value[T]) Get() T {
+	switch state(v.st.Load()) {
+	case stOK:
+		return v.val
+	case stFailed:
+		panic(fmt.Sprintf("module %s initialization failed", v.name))
+	default:
+		panic(fmt.Sprintf("using the %s module before init", v.name))
+	}
+}
